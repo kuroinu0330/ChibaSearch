0462333fdcbf2736398896fed91747f38230e460
Violetkuroinu <21c1020055wt@edu.tech.ac.jp>
2024-01-17 00:08:09 +0900

    レビュー反映　不要のStartメソッドを削除

--- Assets/Scripts/Quiz/ButtonController.cs
+++ Assets/Scripts/Quiz/ButtonController.cs
@@ -7,11 +7,6 @@ public class ButtonController : MonoBehaviour
 {
     [SerializeField]
     private Button _button;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //_button = this.gameObject.GetComponent<Button>();
-    }
     public void InteractiveOn()
     {
         _button.interactable = true;
